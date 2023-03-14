9051c7348b24cfd2a09e3b044f36c9c9e827bfbf
RobertoNesta <robertonesta1993@gmail.com>
2023-03-14 12:56:43 +0100

    adding the for loop

---
+++ jsnack3/assets/js/main.js
@@ -0,0 +1,18 @@
+/*Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.*/
+
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+
+
+let sum = 0
+for (let i = 0; i < 10; i++) {
+    let number = Number(prompt("inserisci un numero"))
+    sum += numb
+}
+ console.log(sum)
+
+
+
+
+
+//Il programma stampa la somma di tutti i numeri inseriti.

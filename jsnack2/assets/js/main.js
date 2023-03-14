3e10abf187eb3c3ad413d4a9ed7e9e1dc80d80e8
RobertoNesta <robertonesta1993@gmail.com>
2023-03-14 12:20:54 +0100

    making the sentence visible on page

--- jsnack2/assets/js/main.js
+++ jsnack2/assets/js/main.js
@@ -7,12 +7,17 @@ const firstWord = prompt("inserisci una parola") ;
 
 const secondWord= prompt("inserisci una seconda parola") ;
 
+let sentence = document.getElementById("sentence")
+
 //Il software stampa prima la parola più corta, poi la parola più lunga.*/
 
 if (firstWord.length > secondWord.length){
     console.log (`la parola più corta è ${secondWord}, quella più lunga è ${firstWord}`)
+    sentence.innerHTML=(`la parola più corta è ${secondWord}, quella più lunga è ${firstWord}`)
 } else if (firstWord.length < secondWord.length) {
     console.log (`la parola più corta è ${firstWord}, quella più lunga è ${secondWord}`)
+    sentence.innerHTML=(`la parola più corta è ${firstWord}, quella più lunga è ${secondWord}`)
 } else{
     console.log (`${firstWord} e ${secondWord} hanno la stessa lunghezza`)
+    sentence.innerHTML=(`${firstWord} e ${secondWord} hanno la stessa lunghezza`)
 }

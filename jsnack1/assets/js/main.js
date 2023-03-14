03e86655060c666b365220807fac10f321551610
RobertoNesta <robertonesta1993@gmail.com>
2023-03-14 11:49:42 +0100

    output number on page

---
+++ jsnack1/assets/js/main.js
@@ -0,0 +1,19 @@
+/*L’utente inserisce due numeri in successione, con due prompt.*/
+
+const firstRandomNumber = Number(prompt("scegli un numero da 1 a 100"))
+
+const secondRandomNumber = Number(prompt("scegli un numero da 1 a 100"))
+
+const output = document.getElementById("output_number")
+
+/*Il software stampa il maggiore.*/
+
+if (firstRandomNumber > secondRandomNumber) {
+    console.log("firstRandomNumber")
+    output.innerHTML = ("firstRandomNumber")
+
+} else {
+    console.log("secondRandomNumber")
+    output.innerHTML = ("secondRandomNumber")
+}
+

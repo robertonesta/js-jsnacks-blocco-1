c75ad6c511a6aae5f2b53b6286bc452a1a47e110
RobertoNesta <robertonesta1993@gmail.com>
2023-03-14 11:59:53 +0100

    changing the second prompt request

--- jsnack1/assets/js/main.js
+++ jsnack1/assets/js/main.js
@@ -2,7 +2,7 @@
 
 const firstRandomNumber = Number(prompt("scegli un numero da 1 a 100"))
 
-const secondRandomNumber = Number(prompt("scegli un numero da 1 a 100"))
+const secondRandomNumber = Number(prompt("scegli un altro numero da 1 a 100"))
 
 const output = document.getElementById("output_number")
 

bdf4f24da4b0d418a93f39c042ebdcd06e8c14c1
RobertoNesta <robertonesta1993@gmail.com>
2023-03-15 12:25:47 +0100

    for loop attempt

---
+++ jsnack4/assets/js/main.js
@@ -0,0 +1,29 @@
+/*Snack 4 (sia con for che con while):
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.*/
+
+let guests = [
+    'marco',
+    'vittorio',
+    'giacomo',
+    'riccardo',
+    'luca',
+    'roberto',
+]
+
+let access = false;
+let guestname = prompt("inserisci qui il tuo nome")
+
+for (let i = 0; i = guests.length; i++){
+    
+    let guest = guests.length[i];
+
+    if (guestname === guest) {
+        access = true;
+    }
+}
+
+if (access){
+    console.log(`ciao ${guestname}, puoi entrare`)
+} else {
+    console.log(`mi dispiace, ${guestname}, non sei tra gli invitati`)
+}

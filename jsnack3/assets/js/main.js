53f2a56c78d594d6fa86e11cf30ba4e2537f8f48
RobertoNesta <robertonesta1993@gmail.com>
2023-03-15 11:51:14 +0100

    redoing snack 3 using the while loop

--- jsnack3/assets/js/main.js
+++ jsnack3/assets/js/main.js
@@ -4,15 +4,27 @@ Il programma stampa la somma di tutti i numeri inseriti.*/
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 
 
-let sum = 0
+/*let sum = 0
 for (let i = 0; i < 10; i++) {
     let number = Number(prompt("inserisci un numero"))
     sum += number
 }
- console.log(sum)
+
+// Il programma stampa la somma di tutti i numeri inseriti.
+ console.log(sum) */
 
 
 
 
+/*Rifare con WHILE LOOP:  Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti. */
 
-//Il programma stampa la somma di tutti i numeri inseriti.
+let i = 0; 
+let sum = 0;
+while (i < 10) {
+    let number = Number(prompt("inserisci un numero"))
+    sum += number
+    i++
+}
+
+console.log(sum)

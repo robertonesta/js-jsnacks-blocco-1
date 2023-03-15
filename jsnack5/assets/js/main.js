7162ff59bd33ce46b0ae8685598b1071fa8f9399
RobertoNesta <robertonesta1993@gmail.com>
2023-03-15 12:49:23 +0100

    while loop solution

--- jsnack5/assets/js/main.js
+++ jsnack5/assets/js/main.js
@@ -1,3 +1,26 @@
 /*Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.*/
+
+
+
+const oddnumbers = ''
+for (i = 0; i < 6; i++){
+    let number = Number(prompt("inserisci un numero"))
+
+    if (number % 2 != 0) {
+        console.log(number)
+    } 
+
+}
+//while loop
+x = 0
+while (x < 6){
+    let number = Number(prompt("inserisci un numero"))
+
+    if (number % 2 != 0) {
+        console.log(number)
+    } 
+    
+    x++
+}

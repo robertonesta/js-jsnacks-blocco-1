3b430fa12015f9a19e2e70e6414ce7b1d9434a63
RobertoNesta <robertonesta1993@gmail.com>
2023-03-30 11:37:29 +0200

    Milestone 1

--- workshop-vuejs/assets/js/app.js
+++ workshop-vuejs/assets/js/app.js
@@ -1,9 +1,33 @@
+/* 
+
+M1:
+Realizzare un applicazione per inserire delle tasks in una lista. 
+La lista avrá delle task iniziali da stampare in pagina.
+
+M2:
+Tramite un input ed pulsante permettiamo all'utente di inserire una nuova task nella lista e vistualizzare la lista aggiornata.
+
+M3:
+L'utente non deve poter inserire una task vuota
+Le task devono avere almento 3-5 caratteri
+
+M4:
+Se l'utente clicca sulla task (o su un pulsante nella task) la puo marcare come completa e rimuoverla dalla lista di task da completare.
+Se l'utente ha completato tutte le task 😎 mostriamo un messaggio
+
+Bonus:
+Man mano che le task vengono completate le visualizziamo in una lista apparte
+
+*/
+
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      
+      tasks:[
+        'Learn HTML', 'Learn CSS', 'Learn JS', 'Learn PHP'
+      ]
     }
   }
 }).mount('#app')
